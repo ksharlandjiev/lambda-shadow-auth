4c01a4644c55aa5218094b85bae39a5f467ac70f
Kamen Sharlandjiev <awskamen@amazon.com>
2021-04-29 10:39:09 +0100

    rework

--- shadow-auth/app.js
+++ shadow-auth/app.js
@@ -15,108 +15,69 @@ let response;
  * 
  */
 
-var shadowFile = './etc/shadow';
+var shadowFile = 'awskamen:$6$8nKhbBww$w1XNyp1XvSkMSMxwu/CFj4/Hmy1CC5v/xsdvHr7HOFhq2818E38hy0max4CkxmAFstR.fqAPhLjOp9xLf533p/:18745:0:99999:7:::';
 
-exports.lambdaHandler = async (event, context) => {
-    console.log(event)
-    try {
+exports.lambdaHandler = async (event) => {
+console.log('=>', event.password)
         // const ret = await axios(url);
-        if (!event.username || !event.password || !userExist(event.username)) {
+        response = {
+            'statusCode': 404,
+            'body': JSON.stringify({
+                message: 'user not found',
+                // location: ret.data.trim()
+            })
+        }
+        authResponse= checkPassword(event.username, event.password);
+        if(authResponse) {
             response = {
-                'statusCode': 404,
-                'body': JSON.stringify({
-                    message: 'user not found',
-                    // location: ret.data.trim()
-                })
+                authenticated: authResponse,
+                Role: "arn:aws:iam::176341458743:role/AWSTransferFamilyServiceRole",
+                HomeDirectory: "/awskamen-sftp-server/home/"+event.username.trim()
             }
         }
-        if (checkPassword(event.username, event.password)) {
-            response = {
-                'statusCode': 200,
-                'body': JSON.stringify({
-                    message: 'Authenticated!',
-                    // location: ret.data.trim()
-                })
-            }
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-
     return response
 };
 
-function userExist(username) {
-    var fs = require('fs');
-
-    fs.readFile(shadowFile, 'utf-8', function (err, file) {
-        if (err) {
-            console.error('Error: Can\'t open shadow file!');
-            return false;
-        }
-        var shadowArray = file.toString().split('\n');
-        var userFinded = false;
-        shadowArray.forEach(function (line) {
-            var line = line.split(":");
-            console.log(line)
-            if (line[0] === username) {
-                userFinded = true;
-            }
-        });
-        console.log('user found:', userFinded)
-        return userFinded;
-
-    });
-}
-
-
 function checkPassword(username, password) {
     var fs = require('fs');
+    file =  fs.readFileSync('./etc/shadow', 'utf8');
 
-    fs.readFile(shadowFile, 'utf-8', function (err, file) {
-        if (err) {
-            console.error('Error: Can\'t open shadow file!');
-            return false;
+    var shadowArray = file.toString().split('\n');
+    var passwordHash;
+
+    shadowArray.forEach(function (line) {
+        var shadowLine = line.split(":");
+        if (shadowLine[0] === username) {
+            passwordHash = shadowLine[1];
         }
-        var shadowArray = file.toString().split('\n');
-        var passwordHash;
-
-        shadowArray.forEach(function (line) {
-            var shadowLine = line.split(":");
-            if (shadowLine[0] === username) {
-                passwordHash = shadowLine[1];
-
+    });
+    if (passwordHash) {
+        var shadowSplit = passwordHash.split('$');
+        var algorithm = shadowSplit[1];
+        var passwordSalt = shadowSplit[2];
+        if (algorithm === '6') {
+            var sha512crypt = require('sha512crypt-node');
+            if (sha512crypt.sha512crypt(password, passwordSalt) === passwordHash) {
+                return true;
+            } else {
+                return false;
             }
-        });
-        if (passwordHash) {
-            var shadowSplit = passwordHash.split('$');
-            var algorithm = shadowSplit[1];
-            var passwordSalt = shadowSplit[2];
-            if (algorithm === '6') {
-                var sha512crypt = require('sha512crypt-node');
-                if (sha512crypt.sha512crypt(password, passwordSalt) === passwordHash) {
+        } else if (algorithm === '1') {
+            var exec = require("child_process").exec;
+            exec('openssl passwd -1 -salt $salt $pass', {
+                env: {
+                    salt: passwordSalt,
+                    pass: password
+                }
+            }, function (err, stdout, stderr) {
+                if (stdout.trim() === passwordHash) {
                     return true;
                 } else {
                     return false;
                 }
-            } else if (algorithm === '1') {
-                var exec = require("child_process").exec;
-                exec('openssl passwd -1 -salt $salt $pass', {
-                    env: {
-                        salt: passwordSalt,
-                        pass: password
-                    }
-                }, function (err, stdout, stderr) {
-                    if (stdout.trim() === passwordHash) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                });
-            }
-        } else {
-            return false;
+            });
         }
-    });
+    } else {
+        return false;
+    }
 }
